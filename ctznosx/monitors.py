7f28ab48140d5b1214201b42ccc30778e1641861
Splug <m@splug.io>
2014-07-22 08:28:17 -0400

    added install monitor and list monitor function

--- ctznosx/monitors.py
+++ ctznosx/monitors.py
@@ -1,20 +1,65 @@
 from __future__ import unicode_literals
+
+import getpass
+from os import walk
+from sys import exit
+from shutil import rmtree
+from os.path import isdir,join
 from titantools.system import shell_out
 import urllib2, urllib, httplib, json
+
+MONITOR_PATH = '/var/lib/ctznosx/'
 
 # TODO: Install checksum check on package
 def install(args):
     PREFIX = "[ Monitor::Install ] "
+
     print PREFIX, "Checking for monitor at: %s" % args[0]
+
+    # Check if it's already installed
+    if does_monitor_exist(args[0]):
+        print PREFIX, "This monitor already exists"       
+        exit(2)
+
+    # Check if it's a valid URL
     if 200 == http_get_module(args[0]):
-        print PREFIX, "Valid HTTP link, lets check it out"
+        print PREFIX, "Valid HTTP link, lets install it"
+        shell_out("cd %s && sudo git clone %s" % (MONITOR_PATH, args[0]))
+        shell_out("sudo chown -R _ctznosx %s" % MONITOR_PATH)
+
     else:
-        print PREFIX, "That is not a valid module"
-    #sys("git %s /var/lib/ctznosx/monitors/")
+        print PREFIX, "That is not a valid module, not installing anything"
+        exit(1)
     
 
+def does_monitor_exist(monitor):
+    # Parse out the monitor from URL assuming Github
+    monitor = monitor.rsplit('/', 1)[-1].split(".")[0]
+
+    if isdir(join(MONITOR_PATH, monitor)):
+        return True
+    else:
+        return False 
+
 def remove(args):
-    print args
+
+    PREFIX = "[ Monitor::Remove ] "
+
+    if getpass.getuser() != 'root':
+        print PREFIX, "Please run this with elevated permissions"
+        exit(1)
+    
+    if does_monitor_exist(args[0]):
+        rmtree(join(MONITOR_PATH, args[0]))
+    else:
+        print "Not a valid monitor"
+        exit(1)
+
+def list(args):
+    for path, dirs, files in walk(path):
+      print path
+      for f in files:
+        print f
 
 # Send the request
 def http_get_module( target ):
@@ -23,7 +68,7 @@ def http_get_module( target ):
     request = urllib2.Request(target)
     opener = urllib2.build_opener()
     response = opener.open(request)
-    response_object = response.getcode(), response
+    response_code = response.getcode()
 
   except urllib2.HTTPError, e:
     if e.code == 307:
@@ -40,4 +85,4 @@ def http_get_module( target ):
   except urllib2.URLError, e:
     return 500
 
-  return response_object    
+  return response_code    
