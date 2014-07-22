1da5df9b2527b5d7ce14d3969d1dd9811c5da7c8
Splug <m@splug.io>
2014-07-22 09:06:57 -0400

    finished monitors submodule

--- ctznosx/monitors.py
+++ ctznosx/monitors.py
@@ -2,10 +2,11 @@ from __future__ import unicode_literals
 
 import getpass
 import datetime
-from glob import glob
-from sys import exit
-from shutil import rmtree
 from os import stat
+from sys import exit
+from glob import glob
+from pwd import getpwuid
+from shutil import rmtree
 from os.path import isdir,join,sep
 from titantools.system import shell_out
 import urllib2, urllib, httplib, json
@@ -16,18 +17,26 @@ MONITOR_PATH = '/var/lib/ctznosx/monitors/'
 def install(args):
     PREFIX = "[ Monitor::Install ] "
 
-    print PREFIX, "Checking for monitor at: %s" % args[0]
-
     # Check if it's already installed
     if does_monitor_exist(args[0]):
         print PREFIX, "This monitor already exists"       
         exit(2)
 
+
     # Check if it's a valid URL
-    if 200 == http_get_module(args[0]):
-        print PREFIX, "Valid HTTP link, lets install it"
+    print PREFIX, "Locating monitor: %s" % args[0]
+    if validate_repo(args[0]):
+
+        # Let's clone it and chown it
+        print PREFIX, "Found monitor: '%s'" % args[0].rsplit('/', 1)[1].split('.')[0]
         shell_out("cd %s && sudo git clone %s" % (MONITOR_PATH, args[0]))
         shell_out("sudo chown -R _ctznosx %s" % MONITOR_PATH)
+
+        # Check that it exists, which means it installed successfully
+        if does_monitor_exist(args[0]):
+            print PREFIX,"Monitor successfully installed"
+        else:
+            print PREFIX,"There was an error installing the monitor"
 
     else:
         print PREFIX, "That is not a valid module, not installing anything"
@@ -52,13 +61,34 @@ def remove(args):
         exit(1)
     
     if does_monitor_exist(args[0]):
-        rmtree(join(MONITOR_PATH, args[0]))
+        monitor_path = join(MONITOR_PATH, args[0])
+        
+        owner = find_owner(monitor_path)
+        if "_ctznosx" != owner:
+            print PREFIX,"Unable to remove, invalid permissions"
+            exit(128)
+        else:
+            rmtree(monitor_path)
+
+        # Make sure it doesnt exist still
+        if does_monitor_exist(args[0]):
+            print PREFIX,"Removing monitor failed"
+        else:
+            print PREFIX,"Successfully remove monitor"
+
     else:
-        print "Not a valid monitor"
+        print PREFIX,"Could not find a monitor by the name of '%s'" % args[0]
         exit(1)
 
 def list(args):
+    print "The following ctznOSX monitors are installed:\n"
+
     monitors = [monitor for monitor in glob( join(MONITOR_PATH,"*") ) if monitor.find('README.md') is -1]
+
+    if len(monitors) == 0:
+        print "None"
+        exit()
+        
     for monitor in monitors:
         stats = stat(monitor)        
         
@@ -69,32 +99,15 @@ def list(args):
 
         sourced_from = shell_out("git -C %s remote -v" % monitor)
 
-        print "The following ctznOSX monitors are installed:\n"
         print "{}\n\tInstalled on: {}\n\tSourced From:   {}".format(monitor_name, install_date, sourced_from.replace("\n", "\n\t\t\t"))
 
+def find_owner(filename):
+    return getpwuid(stat(filename).st_uid).pw_name
 
 # Send the request
-def http_get_module( target ):
+def validate_repo( target ):
+    result = shell_out("git ls-remote %s" % target)
+    if "HEAD" in result:
+        return True
 
-  try:
-    request = urllib2.Request(target)
-    opener = urllib2.build_opener()
-    response = opener.open(request)
-    response_code = response.getcode()
-
-  except urllib2.HTTPError, e:
-    if e.code == 307:
-      for line in str(e.headers).splitlines():
-        if "Location" in line:
-          new_target = line.split(": ", 1)[1]
-          return send_request( new_target )
-    else:
-      return e.code
-  
-  except ValueError, e:
-    return 500
-
-  except urllib2.URLError, e:
-    return 500
-
-  return response_code    
+    return False
