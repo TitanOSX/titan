0954dce1f11561d7c634aeefa00d8fa5058a229d
Splug <m@splug.io>
2014-07-22 07:00:36 -0400

    updated pre/post install scripts for setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import os, sys
 from ctznosx import __version__ as version
 from distutils.core import setup
+from setuptools.command.install import install
+from titantools.system import shell_out
 
 long_description = """
 ctznOSX is used by system administrators and
@@ -12,6 +14,31 @@ install_requires = ['titantools']
 
 if sys.version_info < (2, 7):
     install_requires += ['argparse']
+
+class CtznosxInstaller(install):
+    def preinstall(self):
+        # Add custom pre-install stuff
+        print "Adding ctznOSX User"
+        add_user_script = """dscl . -create /Users/_ctznosx
+dscl . -create /Users/_ctznosx RealName "ctznOSX Management User"
+dscl . -create /Users/_ctznosx UniqueID 403  # Use something between 100 and 500 to hide the user
+dscl . -create /Users/_ctznosx PrimaryGroupID 20
+dscl . -create /Users/_ctznosx UserShell /sbin/nologin
+dseditgroup -o edit -a _ctznosx -t user admin
+dseditgroup -o edit -a _ctznosx -t user wheel"""
+        shell_out(add_user_script)
+
+    def postinstall(self):
+        # Post install script
+        print "Updating permissions for ctznOSX"
+        shell_out("chown -R _ctznosx:_ctznosx /var/lib/ctznosx")
+
+    def run(self):
+
+        self.preinstall()
+        # Call the super run
+        install.run(self)
+        self.postinstall()
 
 setup(
     name='ctznosx',
@@ -37,4 +64,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
     ],
+    cmdclass={
+        'install': CtznosxInstaller,
+    },
 )
