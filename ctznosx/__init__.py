63dbf35f109dc5dc2f88df757cf25f27b86f3138
Splug <m@splug.io>
2014-07-25 14:30:15 -0400

    updating version

--- ctznosx/__init__.py
+++ ctznosx/__init__.py
@@ -4,4 +4,4 @@ from __future__ import unicode_literals
 __author__     = 'Mike Mackintosh'
 __authorslug__ = 'splug'
 __copyright__  = '2014, Mike Mackintosh'
-__version__    = '0.9.8'
+__version__    = '0.9.8b'
