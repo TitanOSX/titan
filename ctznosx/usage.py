880187820b17c0e109c9b6288cde3fd8b35dbf5e
Splug <m@splug.io>
2014-08-23 00:54:36 -0400

    fix usage layout

--- ctznosx/usage.py
+++ ctznosx/usage.py
@@ -3,18 +3,6 @@ from __future__ import unicode_literals
 def _(text):
     return text.strip('\n')
 
-USAGE = _("""
-                                  MMP\"""\""YMM MP\"""\"""`MM M""MMMM""M
-           dP                     M' .mmm. `M M  mmmmm..M M  `MM'  M
-           88                     M  MMMMM  M M.      `YM MM.    .MM
-.d8888b. d8888P d888888b 88d888b. M  MMMMM  M MMMMMMM.  M M  .mm.  M
-88'  `""   88      .d8P' 88'  `88 M. `MMM' .M M. .MMM'  M M  MMMM  M
-88.  ...   88    .Y8P    88    88 MMb     dMM Mb.     .dM M  MMMM  M
-`88888P'   dP   d888888P dP    dP MMMMMMMMMMM MMMMMMMMMMM MMMMMMMMMM
-
-Usage: ctznosx [--config=CONFIG] command
-""")
-
 HELP = _("""
   -h, --help         show help message
   -v, --version      show version
@@ -28,6 +16,19 @@ commands:
   report             display report
 
 """)
+
+USAGE = _("""
+                                  MMP\"""\""YMM MP\"""\"""`MM M""MMMM""M
+           dP                     M' .mmm. `M M  mmmmm..M M  `MM'  M
+           88                     M  MMMMM  M M.      `YM MM.    .MM
+.d8888b. d8888P d888888b 88d888b. M  MMMMM  M MMMMMMM.  M M  .mm.  M
+88'  `""   88      .d8P' 88'  `88 M. `MMM' .M M. .MMM'  M M  MMMM  M
+88.  ...   88    .Y8P    88    88 MMb     dMM Mb.     .dM M  MMMM  M
+`88888P'   dP   d888888P dP    dP MMMMMMMMMMM MMMMMMMMMMM MMMMMMMMMM
+
+Usage: ctznosx [--config=CONFIG] command
+
+%s""" % HELP)
 
 # Unused ATM
 #  clean              clean data store contents
