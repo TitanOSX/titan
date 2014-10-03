6917ef7e741e6c74bd34daa0691b305b42e006c2
Mike Mackintosh <m@splug.io>
2014-10-02 21:52:39 -0400

    fixed device registration and status reporting

--- titan/devicemgmt.py
+++ titan/devicemgmt.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 import datetime
+import json
 from os import environ
 from sys import exit
 from os.path import join
@@ -10,7 +11,7 @@ import urllib2, urllib, httplib, json
 from config import titanConfig
 
 # Get titanOSX Env and Config
-TITAN_PATH = (environ.get('TITAN_CONFIG_PATH') or '/var/lib/titanosx/')
+TITAN_PATH = (environ.get('TITAN_CONFIG_PATH') or '/var/lib/titan/')
 TITAN_CONFIG = join('/etc/', 'titan.conf')
 
 # Config
@@ -20,26 +21,47 @@ CONFIG = titanConfig( TITAN_CONFIG, TITAN_PATH )
 software = s.sw_details()
 hardware = s.hw_details()
 
-# Log Prefix
-PREFIX = "[ Manager::Register ] "
-
 # Reporting Token
 TOKEN = {'token': CONFIG['reporting']['token']}
 
+# Create empty data objects
+data = {}
+data["serial"] = hardware["serial_number"]
+
 """ check status """
 def status():
+    # Log Prefix
+    PREFIX = "[ Manager::Status ] "
     print PREFIX, "Checking status for remote server"
-    code, resp = http.request("%s/status/%s" % (CONFIG["reporting"]["target"], data["serial"]))
-    print resp
+    
+    code, resp = http.request("%s/api/status/%s" % (CONFIG["reporting"]["target"], data["serial"]))
+    
+    if code == 200:
+        print PREFIX, "Device is registered with", CONFIG["reporting"]["target"]
+        device = json.loads(resp)
+        print ""
+        print "Remote ID: %s" % device['id']
+        print "Serial: %s" % device['serial']
+        print "This device is a %s %s with an %s @ %s and %s of memory running %s" % (device['make'], device['model'], device['cpu_type'], device['cpu_speed'], device['physical_memory'], device['os_version'])
+
+    else:
+        print PREFIX, "Device is not registered"
+
+
+"""
+Proposed work flow:
+
+register command reaches out to status, if register, return 304
+if not, follow 307 redirect, and register, return 200
+
+"""
 
 """ register """
 def register():
+    # Log Prefix
+    PREFIX = "[ Manager::Register ] "
     print PREFIX, "Attempting to register device with remote server"
 
-    # Create empty data objects
-    data = {}
-
-    data["serial"] = hardware["serial_number"]
     data["uuid"] = hardware["hardware_uuid"]
     data["make"] = hardware["machine_make"]
     data["model"] = hardware["model_short"]
@@ -50,7 +72,7 @@ def register():
     data["os_build"] = software["os_build"]
 
     # Check status first, autoredirect to register
-    code, resp = send_request("%s/status/%s" % (CONFIG["reporting"]["target"], data["serial"]), data)
+    code, resp = send_request("%s/api/status/%s" % (CONFIG["reporting"]["target"], data["serial"]), data)
     
     if code == 0:
         print PREFIX, "Unable To Communicate With Registration Server"
